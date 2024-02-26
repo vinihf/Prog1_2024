2e68eb3064b618dd50ddcd7a160363128aacd3fa
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-02-26 15:10:55 -0300

    Exercícios resolvido.

---
+++ Introdução/Resolução de exercícios/3.py
@@ -0,0 +1,3 @@
+total_euros = 1250.0
+total_reais = total_euros*5.42
+print(f'O valor de \u20ac{total_euros:.2f} equivale a R${total_reais:.2f}')
---
+++ Introdução/Resolução de exercícios/4a.py
@@ -0,0 +1,4 @@
+pizzas = int(input("Informe quantas pizzas de 12 pedaços serão divididas:"))
+qtdPessoas = int(input("Informe o número de pessoas que irão comer:"))
+divisao = (pizzas*12)/qtdPessoas
+print(f'Se {pizzas} pizzas de 12 pedaços são divididas entre {qtdPessoas} pessoas, cada uma recebe {divisao} pedaços.')
