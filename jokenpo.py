2f7ef2fffd9e181eb10a2272d71f94c6c0f289bd
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2024-03-11 09:38:00 -0300

    Teste

---
+++ jokenpo.py
@@ -0,0 +1,7 @@
+jogada1 = int(input("Informe a sua jogada: (1) Pedra, (2) Papel, (3) Tesoura"))
+jogada2 = int(input("Informe a sua jogada: (1) Pedra, (2) Papel, (3) Tesoura"))
+
+if jogada1 == jogada2:
+    print("O jogo empatou.")
+else:
+    if
